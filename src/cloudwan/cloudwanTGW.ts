546f82e8cceaf7709adedd56151412137d1472e9
Andrew Frazer <andrew.frazer@raindancers.cloud>
2022-11-27 20:01:39 +1300

    fix: permissions in lambda

--- src/cloudwan/cloudwanTGW.ts
+++ src/cloudwan/cloudwanTGW.ts
@@ -538,7 +538,7 @@ export class CloudWanTGW extends constructs.Construct {
     createVpnCrLambda.addToRolePolicy(
       new iam.PolicyStatement({
         effect: iam.Effect.ALLOW,
-        resources: [`*`],
+        resources: ['*'],
         actions: [
           'ec2:CreateVpnConnection',
           'ec2:DeleteVpnConnection',
