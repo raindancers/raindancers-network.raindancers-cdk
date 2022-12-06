db8217fa8f584d6636dab776ca7857a545ffa544
Andrew Frazer <andrew.frazer@raindancers.cloud>
2022-12-06 20:53:35 +1300

    fix: ids

--- src/evpc/enterprisevpc.ts
+++ src/evpc/enterprisevpc.ts
@@ -428,7 +428,7 @@ export class EnterpriseVpc extends constructs.Construct {
         props.cidr.forEach((destinationCidr) => {
           this.vpc.selectSubnets({ subnetGroupName: subnetGroup }).subnets.forEach(subnet => {
 
-            new ec2.CfnRoute(this, 'FirewallRoute-' + subnet.node.path.split('/').pop() + destinationCidr + hashProps(props), {
+            new ec2.CfnRoute(this, 'FirewallRoute-' + subnet.node.path.split('/').pop() + hashProps(props), {
               destinationCidrBlock: destinationCidr,
               routeTableId: subnet.routeTable.routeTableId,
               vpcEndpointId: fwDescription.getResponseField(`FirewallStatus.SyncStates.${subnet.availabilityZone}.Attachment.EndpointId`),
